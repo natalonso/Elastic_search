9b9dbea7312ae3d4ec571128f30c4b23bb94fb82
natalia <natalia1528alonso@gmail.com>
2020-06-04 17:25:58 +0200

    carga datos, primeras consultas

--- datosdentrobulk.sh
+++
@@ -1,7 +0,0 @@
-
-
-curl -s -XPOST 'localhost:9200/dbpl/_bulk' -H "Content-Type: application/json" --data-binary "@C:\Users\natal\PycharmProjects\Recuperacion_info\elastic.json"
-
-curl -XPUT 'localhost:9200/dbpl' -H 'Content-Type: application/json'
-
-
